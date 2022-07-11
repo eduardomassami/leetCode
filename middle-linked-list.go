28d39466895369f690ce3102dba57865a4e99c1b
Eduardo Massami Uchida <massami.uchida@gmail.com>
2022-07-11 00:51:30 -0300

    middle linked list

--- middle-linked-list.go
+++ middle-linked-list.go
@@ -9,17 +9,21 @@ type ListNode struct {
 
 func main() {
 
-	var input1, input2, input3, input4, input5 ListNode
+	var input1, input2, input3, input4, input5, input6 ListNode
 	// var input1, input2, input3, input4 ListNode
 	input1 = ListNode{Val: 1, Next: &input2}
 	input2 = ListNode{Val: 2, Next: &input3}
 	input3 = ListNode{Val: 3, Next: &input4}
 	input4 = ListNode{Val: 4, Next: &input5}
-	input5 = ListNode{Val: 5}
+	input5 = ListNode{Val: 5, Next: &input6}
+	input6 = ListNode{Val: 6}
 	// input4 = ListNode{Val: 4}
 
 	result := middleNode(&input1)
 	fmt.Println(result.Val)
+
+	resultSlice := middleNodeSlice(&input1)
+	fmt.Println(resultSlice.Val)
 
 }
 
@@ -36,4 +40,16 @@ func middleNode(head *ListNode) *ListNode {
 	}
 
 	return slow
+}
+
+func middleNodeSlice(head *ListNode) *ListNode {
+	var nodes []*ListNode
+
+	for head != nil {
+		nodes = append(nodes, head)
+		head = head.Next
+	}
+
+	fmt.Println(len(nodes))
+	return nodes[len(nodes)/2]
 }
